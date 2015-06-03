dd8409c15178bf9a9e348c74dae371cc14a6ca6b
giuseppebianchi <gppbianchi@gmail.com>
2015-06-03 20:42:59 +0200

    debug

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ SC.initialize({
 $('#connect').click(function(){
 	SC.connect(function() {
 	  SC.get('/me/activities', function(me) { 
-	    window.open(JSON.stringify(me, null, 4)); 
+	    console.log(JSON.stringify(me, null, 4)); 
 	  });
 	});
 });
