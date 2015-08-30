03c4e6afc52d9bde81105b43594aa4a93388b0b7
giuseppebianchi <gppbianchi@gmail.com>
2015-08-30 23:05:33 +0200

    let's try

--- js/views/pages/LoginView.js
+++ js/views/pages/LoginView.js
@@ -34,7 +34,8 @@ define(function(require) {
     
     SCconnect: function(){
 	    // initiate auth popup
-		SC.connect(function() {
+		SC.connect(function(result) {
+			console.log(result);
 		  SC.get('/me', function(me) {
 		    localStorage.setItem("account", JSON.stringify(me));
 		    Backbone.history.navigate("start", {trigger: true});
